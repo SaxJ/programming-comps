fa341739ccc37ed9f90bbe0c4ed68e4cb376127c
Saxon Jensen <saxon.jensen@healthengine.com.au>
2018-06-07 16:51:23 +0800

    More euler 17

--- hackerrank/euler/euler17/words.hs
+++ hackerrank/euler/euler17/words.hs
@@ -11,17 +11,39 @@ main = do
 
 process = do
         n <- readLn
-        print $ toWords n
+        print $ digitGroups n 4
+        print $ [groupToWords x | x <- digitGroups n 4]
+        putStrLn $ toWords n
 
 toWords :: Int -> String
-toWords n = show n
+toWords n = let
+    g = digitGroups n 4
+    groups = [groupToWords x | x <- g]
+    build (a, b) ac = let
+        pre = b ++ " " ++ (bigs !! a)
+        in
+            if (g !! a) /= 0 then pre ++ ac else ac
+    in
+        foldr build ""  $ zip [0..] groups
 
 smalls = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
 
-tens = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+tensWords = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
 
 bigs = ["", "Thousand", "Million", "Billion"]
 
-digitGroups n nGroups = addGroup $ abs n
+digitGroups n nGroups = addGroup 4 $ abs n
     where
-        addGroup x = (x `mod` 1000):(addGroup (x `div` 1000))
+        addGroup 0 _ = []
+        addGroup i x = (x `mod` 1000):(addGroup (i - 1) (x `div` 1000))
+
+groupToWords n = let
+    hundreds = n `div` 100
+    nTens = n `mod` 100
+
+    tens = nTens  `div` 10
+    units = nTens `mod` 10
+
+    hundredText = if hundreds /= 0 then "" else (smalls !! hundreds) ++ " Hundred"
+    in
+        if tens >= 2 then hundredText ++ (tensWords !! tens) ++ (if units /= 0 then smalls !! units else "") else (if nTens /= 0 then hundredText ++ (smalls !! nTens) else "")
