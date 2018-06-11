982b11bc75fa65fa9d82b15d98befaf6c79d6e4f
Saxon Jensen <saxon.jensen@healthengine.com.au>
2018-06-11 17:01:20 +0800

    Euler 17 has some subtle problem. But at least it's 90% there now.

--- hackerrank/euler/euler17/words.hs
+++ hackerrank/euler/euler17/words.hs
@@ -11,22 +11,17 @@ main = do
 
 process = do
         n <- readLn
-        print $ digitGroups n 4
-        print $ [groupToWords x | x <- digitGroups n 4]
         putStrLn $ toWords n
 
 toWords :: Int -> String
+toWords 0 = "Zero"
 toWords n = let
-    g = digitGroups n 4
-    groups = [groupToWords x | x <- g]
-    build (a, b) ac = let
-        pre = b ++ " " ++ (bigs !! a)
-        in
-            if (g !! a) /= 0 then pre ++ ac else ac
+    groups = reverse $ digitGroups n 4
+    make g i = if g == 0 && i /= 3 then join $ ["",""] else join $ [(groupToWords g), (bigs !! (3-i))]
     in
-        foldr build ""  $ zip [0..] groups
+        join $ zipWith make groups [0..]
 
-smalls = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+smalls = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
 
 tensWords = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
 
@@ -38,12 +33,17 @@ digitGroups n nGroups = addGroup 4 $ abs n
         addGroup i x = (x `mod` 1000):(addGroup (i - 1) (x `div` 1000))
 
 groupToWords n = let
-    hundreds = n `div` 100
-    nTens = n `mod` 100
+    nHundreds = n `div` 100
+    hundreds = if nHundreds > 0 then (smalls !! nHundreds) ++ " Hundred" else ""
 
-    tens = nTens  `div` 10
-    units = nTens `mod` 10
-
-    hundredText = if hundreds /= 0 then "" else (smalls !! hundreds) ++ " Hundred"
+    left = n `mod` 100
+    nTens = left `div` 10
+    tens = if nTens > 2 then join $ [(tensWords !! nTens), (smalls !! (left `mod` 10))] else smalls !! left
     in
-        if tens >= 2 then hundredText ++ (tensWords !! tens) ++ (if units /= 0 then smalls !! units else "") else (if nTens /= 0 then hundredText ++ (smalls !! nTens) else "")
+        unwords $ if nHundreds > 0 then [hundreds, tens] else [tens]
+
+join :: [String] -> String
+join xs = let
+    opp s acc = if length s > 0 then s ++ " " ++ acc else acc
+    in
+        reverse $ dropWhile (== ' ') $ reverse $ foldr opp "" xs
