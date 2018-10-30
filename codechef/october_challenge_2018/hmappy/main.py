17c3c5fa1f288d2d59228ce6f81875e338932c5e
Saxon Jensen <saxon.jensen@healthengine.com.au>
2018-10-30 13:06:54 +0800

    Another programming comp and another euler question.

---
+++ codechef/october_challenge_2018/hmappy/main.py
@@ -0,0 +1,5 @@
+n, numBalloons = [int(x) for x in input().split()]
+wanted = [int(x) for x in input().split()]
+candiesPer = [int(x) for x in input().split()]
+
+
--- hackerrank/euler/euler22/main.hs
+++ hackerrank/euler/euler22/main.hs
@@ -1,22 +1,25 @@
 import Control.Monad
 import Data.Char
+import Data.List
 import qualified Data.Map.Strict as Map
 
 main :: IO ()
 main = do
     nNames <- readLn
     names <- replicateM nNames getLines
+    let multipliers = Map.fromList $ zip (sort names) [1..]
     nQueries <- readLn
-    replicateM_ nQueries $ runQuery $ makeMap names
+    replicateM_ nQueries (runQuery multipliers)
 
 getLines :: IO String
 getLines = do
     line <- getLine
     return line
 
-makeMap :: [String] -> Map.Map String Int
-makeMap names =
-    foldr toMap Map.empty names
-    where
-        score w = 
-        toMap mp name = Map.insert name $ score name
+score :: String -> Int
+score word = foldr (\c t -> ((ord c) - 64) + t) 0 word
+
+runQuery :: Map.Map String Int -> IO ()
+runQuery multipliers = do
+    word <- getLine
+    print ((score word) * (multipliers Map.! word))
