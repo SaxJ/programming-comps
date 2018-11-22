bcd8d548cf8d8b8fcda2b8b3a5fe3604e7984fc6
Saxon Jensen <saxon.jensen@healthengine.com.au>
2018-11-22 14:59:55 +0800

    Project euler 23

--- codechef/nov18/prdrg/main.hs
+++ codechef/nov18/prdrg/main.hs
@@ -9,4 +9,13 @@ main = do
 process :: [Int] -> [String]
 process xs = foldr calc [] $ tail xs
     where
-        calc x acc = (unwords ["1", show (2 ^ x)]): acc
+        calc x acc = (unwords [show a, show b]):acc
+            where
+                (a, b) = pattern x
+
+pattern :: Int -> (Int, Int)
+pattern 1 = (1, 2)
+pattern 2 = (1, 4)
+pattern n = if even n then (a, b * 2) else (b + a, 2 * b)
+    where
+        (a, b) = pattern (n - 1)
